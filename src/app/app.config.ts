0bcf85a82128cf5ca28321a570a5732977ed16df
DominikTroendle <domi.troendle@gmail.com>
2025-10-17 13:22:28 +0200

    added firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          "projectId":"danotes-37e5d",
+          "appId":"1:181031662999:web:3e10b28e3ff775ebf7c0d2",
+          "storageBucket":"danotes-37e5d.firebasestorage.app",
+          "apiKey":"AIzaSyDxUrgHoZnEGKi19m_IE2476UXOX7gSi2w",
+          "authDomain":"danotes-37e5d.firebaseapp.com",
+          "messagingSenderId":"181031662999",
+          // "projectNumber":"181031662999",
+          // "version":"2"
+        }
+      ))
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
