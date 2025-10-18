3cbb67b3c66f77a1aa599d06939824db18a81c03
DominikTroendle <domi.troendle@gmail.com>
2025-10-18 09:12:32 +0200

    added different ways to retrieve data from firestore database

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, collectionData, doc, onSnapshot } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubList;
+  unsubSingle;
+
+  items;
+  firestore: Firestore = inject(Firestore);
+  
+  constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element);
+      });
+    });
+    this.unsubSingle = onSnapshot(this.getSingleDocRef('notes', 'AkpL97d0oIDK19q6tu6H'), (el) => {
+      console.log(el);
+    });
+    this.unsubList();
+    this.unsubSingle();
+    this.items = collectionData(this.getNotesRef());
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
